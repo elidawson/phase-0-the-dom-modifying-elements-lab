eb4e78548d4f7933e3f117b1d59871c8b2187bf8
Eli Dawson <edawson@alumni.berklee.edu>
2023-03-27 22:21:38 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Eli is the champion';
+
+document.body.append(h1);
